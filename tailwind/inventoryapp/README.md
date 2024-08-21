6a7c6c6e2b9d003f3413195fcbd360c0b8460d20
anashr18 <anand.shri18@gmail.com>
2024-08-21 11:28:39 +0530

    inventory app before flexbox

---
+++ tailwind/inventoryapp/README.md
@@ -0,0 +1,16 @@
+# install npm packages
+
+Creating app 
+
+`npx create-next-app@latest`
+renamed the project name to client
+
+installing material UI etc
+`npm i @mui/x-data-grid @mui/material @motion/react @motion/style lucide-react numeral recharts uuid axios`
+add types
+`npm i -D @types/node @types/uuid @types/numeral`
+
+
+deleted everything from page.tsx in main tag
+`npm run dev`
+`npm i -D tw-colors`
---
+++ tailwind/inventoryapp/client/tailwind.config.ts
@@ -0,0 +1,48 @@
+import type { Config } from "tailwindcss";
+import { createThemes } from "tw-colors";
+import colors from "tailwindcss/colors";
+
+const baseColors = [
+  "gray",
+  "red",
+  "yellow",
+  "green",
+  "blue",
+  "indigo",
+  "purple",
+  "pink",
+];
+
+const shadeMapping = {
+  "50": "900",
+  "100": "800",
+  "200": "700",
+  "300": "600",
+  "400": "500",
+  "500": "400",
+  "600": "300",
+  "700": "200",
+  "800": "100",
+  "900": "50",
+};
+
+
+
+const config: Config = {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      backgroundImage: {
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+    },
+  },
+  plugins: [],
+};
+export default config;
